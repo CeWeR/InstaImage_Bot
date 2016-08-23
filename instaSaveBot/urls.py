1db6caae479c5a41306e37d30b5ece4d50e385e5
SanzharAmirzhan <amir.sanzhar@gmail.com>
2016-08-23 23:45:57 +0600

    r

--- instaSaveBot/urls.py
+++ instaSaveBot/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^instasave/', include('instaSave.urls')),
+    url(r'^instasave/', include('instaSave.urls', namespace='instasave')),
 ]
